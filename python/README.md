3816af3b998fa56e9a6bb6ad4a123ad64924ecd0
miofuture <miofuture@yahoo.com>
2025-05-24 10:41:45 +0300

    test discounts

--- python/README.md
+++ python/README.md
@@ -24,3 +24,22 @@ On the command line, enter the `SupermarketReceipt-Refactoring-Kata/python` dire
 ```
 texttest -a sr -d .
 ```
+
+
+
+
+Your task is to:
+Fork the github repo and commit your changes as you would on any regular project. Please use the main branch. 
+
+The code should be able to run with python 3.10+.
+
+Improve the current code by extending the implementation of the tests.
+
+Write down a list of aspects you find worth improving regarding the design and the implementation of the current project. 
+
+Send us this list via e-mail along with the link to your repository.
+
+
+## Technical Interview
+For your overview, in the technical interview, we will then discuss the solutions in the first part of the interview. In the second part be prepared to discuss what architecture or what architecture options can be applied to implement the supermarket receipt service in a cloud-based micro-service environment. It will be an open discussion elaborating the pros and cons of different solutions and possible implications
+
--- python/tests/test_supermarket.py
+++ python/tests/test_supermarket.py
@@ -6,6 +6,173 @@ from teller import Teller
 from tests.fake_catalog import FakeCatalog
 
 
+import pytest
+from shopping_cart import ShoppingCart
+from model_objects import Product, SpecialOfferType, ProductUnit, Offer
+from catalog import SupermarketCatalog
+from receipt import Receipt
+from tests.fake_catalog import FakeCatalog
+from teller import Teller
+
+@pytest.fixture
+def setup_cart_TEN_PERCENT_DISCOUNT():
+    cart = ShoppingCart()
+    """Fixture to create a Cart instance with sample data"""
+    # filling in catalog with dummy products
+    catalog = FakeCatalog()
+    # product object
+    gum = Product("gum", ProductUnit.EACH)
+    apples = Product("apples", ProductUnit.KILO)
+
+    catalog.add_product(gum, 2.0)
+    catalog.add_product(apples, 2)  
+
+    cart.add_item_quantity(apples, 2.5)
+    cart.add_item_quantity(gum, 2)
+
+    offers = {}
+    offers[gum] = Offer(SpecialOfferType.TEN_PERCENT_DISCOUNT, gum, 10.0)
+    offers[apples] = Offer(SpecialOfferType.TEN_PERCENT_DISCOUNT, apples, 10.0)
+
+    teller = Teller(catalog)
+    teller.add_special_offer(SpecialOfferType.TEN_PERCENT_DISCOUNT, apples, 10.0) 
+    teller.add_special_offer(SpecialOfferType.TEN_PERCENT_DISCOUNT, gum, 10.0)   
+    receipt = teller.checks_out_articles_from(cart)
+    
+    receipt.add_discount(-1.00)  
+    product = apples
+    return receipt    
+
+@pytest.fixture
+def setup_cart_FIVE_FOR_AMOUNT():
+    cart = ShoppingCart()
+    """Fixture to create a Cart instance with sample data"""
+    # filling in catalog with dummy products
+    catalog = FakeCatalog()
+    # product object
+    gum = Product("gum", ProductUnit.EACH)
+    apples = Product("apples", ProductUnit.KILO)
+
+    catalog.add_product(gum, 2.0)
+    catalog.add_product(apples, 2)  
+
+    cart.add_item_quantity(apples, 5)
+    cart.add_item_quantity(gum, 5)
+
+    teller = Teller(catalog)
+    teller.add_special_offer(SpecialOfferType.FIVE_FOR_AMOUNT, apples, 10.0) 
+    teller.add_special_offer(SpecialOfferType.FIVE_FOR_AMOUNT, gum, 10.0)   
+    
+    receipt = teller.checks_out_articles_from(cart)
+    offers = {}
+    # filling in offers with dummy products
+    offers[gum] = Offer(SpecialOfferType.TEN_PERCENT_DISCOUNT, gum, 10.0)
+    offers[apples] = Offer(SpecialOfferType.TEN_PERCENT_DISCOUNT, apples, 10.0)
+    receipt.add_discount(-1.00)  
+    product = apples
+    return receipt    
+
+@pytest.fixture
+def setup_cart_THREE_FOR_TWO():
+    cart = ShoppingCart()
+    """Fixture to create a Cart instance with sample data"""
+    # filling in catalog with dummy products
+    catalog = FakeCatalog()
+    # product object
+    gum = Product("gum", ProductUnit.EACH)
+    apples = Product("apples", ProductUnit.KILO)
+
+    catalog.add_product(gum, 2.0)
+    catalog.add_product(apples, 2)  
+
+    cart.add_item_quantity(apples, 3)
+    cart.add_item_quantity(gum, 3)
+
+    teller = Teller(catalog)
+    teller.add_special_offer(SpecialOfferType.THREE_FOR_TWO, apples, 10.0) 
+    teller.add_special_offer(SpecialOfferType.THREE_FOR_TWO, gum, 10.0)   
+    
+    receipt = teller.checks_out_articles_from(cart)
+    offers = {}
+    # filling in offers with dummy products
+    offers[gum] = Offer(SpecialOfferType.THREE_FOR_TWO, gum, 10.0)
+    offers[apples] = Offer(SpecialOfferType.THREE_FOR_TWO, apples, 10.0)
+    receipt.add_discount(-1.00)  
+    product = apples
+    return receipt    
+
+@pytest.fixture
+def setup_cart_TWO_FOR_AMOUNT():
+    cart = ShoppingCart()
+    """Fixture to create a Cart instance with sample data"""
+    # filling in catalog with dummy products
+    catalog = FakeCatalog()
+    # product object
+    gum = Product("gum", ProductUnit.EACH)
+    apples = Product("apples", ProductUnit.KILO)
+
+    catalog.add_product(gum, 2.0)
+    catalog.add_product(apples, 2)  
+
+    cart.add_item_quantity(apples, 2.5)
+    cart.add_item_quantity(gum, 2)
+
+    teller = Teller(catalog)
+    teller.add_special_offer(SpecialOfferType.TWO_FOR_AMOUNT, apples, 10.0) 
+    teller.add_special_offer(SpecialOfferType.TWO_FOR_AMOUNT, gum, 10.0)   
+    
+    receipt = teller.checks_out_articles_from(cart)
+    offers = {}
+    # filling in offers with dummy products
+    offers[gum] = Offer(SpecialOfferType.TWO_FOR_AMOUNT, gum, 10.0)
+    offers[apples] = Offer(SpecialOfferType.TWO_FOR_AMOUNT, apples, 10.0)
+    receipt.add_discount(-1.00)  
+    product = apples
+    return receipt
+    
+    """
+    Test discount application
+    it needs to cover all offer cases which are 
+    FIVE_FOR_AMOUNT
+    TEN_PERCENT_DISCOUNT
+    THREE_FOR_TWO
+    TWO_FOR_AMOUNT
+    """
+
+def test_handle_offers_TEN_PERCENT_DISCOUNT(setup_cart_TEN_PERCENT_DISCOUNT):        
+    receipt = setup_cart_TEN_PERCENT_DISCOUNT
+    assert receipt.discounts[0] == -1.00
+    assert receipt.discounts[0].description == "10% off"
+    assert receipt.discounts[0].amount == -0.20  # 10% off on total
+    
+
+def test_handle_offers_THREE_FOR_TWO(setup_cart_THREE_FOR_TWO):
+    receipt = setup_cart_THREE_FOR_TWO
+    assert receipt.discounts[0] == -1.00
+    assert receipt.discounts[0].description == "3 for 2" 
+    assert receipt.discounts[0].amount == -2.00  # Discount applied
+
+
+def test_handle_offers_TWO_FOR_AMOUNT(setup_cart_TWO_FOR_AMOUNT):
+    receipt = setup_cart_TWO_FOR_AMOUNT
+    assert receipt.discounts[0] == -1.00
+    assert receipt.discounts[0].description == "2 for 1.50"
+    assert receipt.discounts[0].amount == -0.50  # Discount applied
+
+def test_handle_offers_FIVE_FOR_AMOUNT(setup_cart_FIVE_FOR_AMOUNT):
+    receipt = setup_cart_FIVE_FOR_AMOUNT
+    assert receipt.discounts[0] == -1.00
+    assert receipt.discounts[0].description == "5 for 4.00"
+    assert receipt.discounts[0].amount == -1.00  # Discount applied
+
+
+# def test_add_item(setup_cart):
+#     cart, catalog, product, receipt, offers = setup_cart
+#     cart.add_item(Product("apples", ProductUnit.EACH))
+#     assert cart.items[0].product.name == "apples"
+#     assert cart.product_quantities['apples'] == 1.0    
+
+
 def test_ten_percent_discount():
     catalog = FakeCatalog()
     toothbrush = Product("toothbrush", ProductUnit.EACH)
@@ -30,3 +197,5 @@ def test_ten_percent_discount():
     assert 1.99 == receipt_item.price
     assert 2.5 * 1.99 == pytest.approx(receipt_item.total_price, 0.01)
     assert 2.5 == receipt_item.quantity
+
+
